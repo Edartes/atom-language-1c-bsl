d604c95a280ca5f9ef1dc5caf8b3088de6971779
Артур Аюханов <aartbear@gmail.com>
2016-02-10 09:55:53 +0300

    Добавлена установка автодополнения из https://github.com/xDrivenDevelopment/1c-syntax/issues/50#issuecomment-181997844

--- developers.start.cmd
+++ developers.start.cmd
@@ -1,5 +1,14 @@
 @echo off
 
+cd %userprofile%\.atom\packages
+
+git clone https://github.com/nixel2007/autocomplete-plus.git
+cd autocomplete-plus
+git checkout fix-76-unicode-support
+git pull
+
+cmd /c "apm install --dev"
+
 if not exist %USERPROFILE%\.atom\dev\packages\language-1c-bsl apm develop language-1c-bsl
 
 cd /d %USERPROFILE%\.atom\dev\packages\language-1c-bsl
